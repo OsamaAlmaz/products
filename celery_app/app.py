20359281535e79d946327423ce202bd7189221f4
Osama Al Mazloum <osama.almazloum@myhost.local>
2020-12-09 22:10:36 -0500

    PI-12

--- celery_app/app.py
+++ celery_app/app.py
@@ -20,3 +20,4 @@ class Database():
 
 URL = 'https://www.walmart.com/ip/ASUS-VivoBook-Flip-14-Thin-Light-2-in-1-Laptop-14-FHD-Intel-Core-i5-8265U-8GBDDR4-RAM-512GB-SSD-Glossy-Touch-Fingerprint-Reader-Windows-10-Star-Grey-/501082588'
 
+X  M
