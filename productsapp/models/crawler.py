ed52691182f50b9ae59646ebf581b44276a5a6a9
Osama Al Mazloum <osama.almazloum@myhost.local>
2020-12-14 09:35:30 -0500

    PI-16

--- productsapp/models/crawler.py
+++ productsapp/models/crawler.py
@@ -11,8 +11,23 @@ from sqlalchemy import relationship
 from base import BaseModel
 from base import Base
 
+
+
 class Crawler(Base):
     __tablename__ = 'crawler'
     id = Column(BigInteger, primary_key= True)
     website_id = Column(String(length=True), ForeignKey("website.id"), ondelete="CASCADE")
-    url = Column (String(length=True), )
+    url = Column (String(length=True), nullable=False)
+
+    def __eq__(self, other):
+        return 
+
+    def __repr__(self):
+        return "Crawler id={}, website_id={}, url={}".format(self.id, self.website_url, self.url)
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return self.website_id
+    
