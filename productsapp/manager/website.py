37c214243c2903f8a37d72fb38d3245a2326f8ea
Osama Al Mazloum <osama.almazloum@myhost.local>
2020-12-20 17:16:11 -0500

    PI-18

--- productsapp/manager/website.py
+++ productsapp/manager/website.py
@@ -1,15 +1,43 @@
 from manager.base import BaseAbstract
 from data_access.crawler import  Website
 from sqlalchemy.orm import Session
+import datetime
+from models.main import Website
 
 
+"""
+class Website(Base):
+    __tablename__ = 'website'
+    id = Column (BigInteger, primary_key=True)
+    website_name = Column (String(length=True), nullable=False)
+    website_base_url = Column(String(length=True), nullable=False)
+"""
 
 class WebsiteManager(BaseAbstract):
-    _data_access = Website
-    #descide if you are planning to do any inheritance here. 
     def __init__ (self, session: Session):
         self._session = session
     
-    # make a private create methods for creating a new Crawler for the system that we can later trigger. 
-    def _create(self, website ) -> Crawler:
+    def create(self, website_name, website_base_url) -> Website:
+        website = self._create(website_name, website_base_url)
+        self._session.commit(website)
+        return website
+    
+    def _create(self, website_name, website_base_url ) -> Website:
+        # put any restrictions to the website name. 
+        date_now = datetime.datetime.utcnow()
+        website = Website(
+            website_name= website_name,
+            website_base_url = website_base_url
+        )
+        self._session.add(website)
+        self._session
+        return website
         
+    def get(self, id:int):
+        q = self._session.query(Website).filter(Website.id=id)
+        return q
+
+    def update_name(self, id: int, name, base_url):
+        
+        q = self._sesison.query(Website).
+        filter(Website.id=id).update({Website.name=name})
